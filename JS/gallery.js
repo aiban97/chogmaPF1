ca2d909979a2a83b4e4f373221f224f1be7044b4
AIBANROI JYRWA <52788785+aiban97@users.noreply.github.com>
2025-03-14 11:14:57 +0530

    fix1

--- JS/gallery.js
+++ JS/gallery.js
@@ -3,8 +3,8 @@ let photosHTML = '';
 photos.forEach((photo) => {
   photosHTML +=`
     <div class="gallery-img-con">
-      <div class="image">
-        <img class="pf-img" src="${photo.image}" alt="" />
+      <div>
+        <img src="${photo.image}" alt="" />
       </div>
       <div class="image-title">${photo.title}</div>
     </div>`;
