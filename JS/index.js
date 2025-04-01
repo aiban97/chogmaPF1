ddd07fbaaae0ce5b66f21758129f3e0418d64620
AIBANROI JYRWA <52788785+aiban97@users.noreply.github.com>
2025-04-01 22:27:32 +0530

    scroll animation

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,7 @@
 let deptHTML = '';
 
 depts.forEach((dept) => {
-  deptHTML +=`<div class="dept-img-con">
+  deptHTML +=`<div class="dept-img-con hidden">
         <a href="${dept.link}" target='_blank'><img src="${dept.image}" alt="" /></a>
       </div>`;
 });
