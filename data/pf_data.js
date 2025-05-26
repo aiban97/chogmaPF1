c5bdc155cb7092726ed7ab9e9c3ba0094b6c3cb0
AIBANROI JYRWA <52788785+aiban97@users.noreply.github.com>
2025-05-26 15:50:09 +0530

    update1.0

---
+++ data/pf_data.js
@@ -0,0 +1,34 @@
+const depts = [
+  {
+    link: '',
+    image: 'img/MB.jpg'
+  },
+  {
+    link: '',
+    image: 'img/WD.jpg'
+  },
+  {
+    link: '',
+    image: 'img/YD.jpg'
+  },
+  {
+    link: '',
+    image: 'img/ED.jpg'
+  },
+  {
+    link: '',
+    image: 'img/SSCD.jpg'
+  },
+  {
+    link: '',
+    image: 'img/CSP.jpg'
+  },
+  {
+    link: '',
+    image: 'img/JC.jpg'
+  },
+  {
+    link: 'https://nicholsroybiblecollege.org/',
+    image: 'img/NBC.jpg'
+  }
+];
